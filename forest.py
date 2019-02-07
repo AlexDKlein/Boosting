d3c929a563e028085a2127842a16bb1d13830f02
Alex Klein <alexdavidklein@gmail.com>
2019-02-07 13:24:39 -0800

    Add forest.py

---
+++ forest.py
@@ -0,0 +1,9 @@
+import numpy as np
+from bootstrap import Bootstrapper
+from tree import Tree
+
+class RandomForest(Bootstrapper):
+    def __init__(self, n_estimators=500, metric='entropy', max_depth=3, min_leaf_size=1, classifier=True):
+        super().__init__(Tree, n_estimators, metric=metric, max_depth=max_depth, 
+                        min_leaf_size=min_leaf_size, classifier=classifier)
+    
